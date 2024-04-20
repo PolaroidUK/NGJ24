3278ce882ae3e9ba4f2ba5aa34b5f90712a284e4
Neil Robert McGuiness <neilmcguiness91@gmail.com>
2024-04-20 12:07:04 +0200

    Created Global Event Manager and began UI Manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    GlobalEventManager _globalEventManager;
+
+    public void Initialize (GlobalEventManager globalEventManager)
+    {
+        
+    }
+
+    private void DecreaseHealth(object null)
+    {
+
+    }
+    
+}
