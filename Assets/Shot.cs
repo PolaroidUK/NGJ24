3d26f53ccdd474b3e3f160807ab43726391a9663
Pelle Bloch Atkinson <stillconnecting@gmail.com>
2024-04-20 12:58:41 +0200

    Addded changing balls

--- Assets/Shot.cs
+++ Assets/Shot.cs
@@ -6,16 +6,43 @@ using UnityEngine;
 public class Shot : MonoBehaviour
 {
     public bool isDamaging;
+    private SpriteRenderer sp;
     [SerializeField] private float speed = 4;
+
+    private void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+    }
+
     public void Shoot(Vector2 vel)
     {
         GetComponent<Rigidbody2D>().velocity = vel * speed;
+        
+        sp.color = isDamaging ? Color.red : Color.green;
     }
     private void OnCollisionEnter2D(Collision2D col)
     {
-      
-        Vector2 inDirection = GetComponent<Rigidbody2D>().velocity;
-        Vector2 inNormal = col.contacts[0].normal;
-        Vector2 newVelocity = Vector2.Reflect(inDirection, inNormal) * speed;
+        if (col.gameObject.CompareTag("Player"))
+        {
+            if (isDamaging)
+            {
+                
+                col.gameObject.GetComponent<PlayerController>().DealDamage();
+            }
+            else
+            {
+    
+                col.gameObject.GetComponent<PlayerController>().HealDamage();
+            }
+
+            Destroy(gameObject);
+        }
+
+        isDamaging = !isDamaging;
+        sp.color = isDamaging ? Color.red : Color.green;
+        //Vector2 inDirection = GetComponent<Rigidbody2D>().velocity;
+        //Vector2 inNormal = col.contacts[0].normal;
+        //Vector2 newVelocity = Vector2.Reflect(inDirection, inNormal) * speed;
+        //GetComponent<Rigidbody2D>().velocity = newVelocity;
     }
 }
