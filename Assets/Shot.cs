f948cb6f9dd922038675d405b6da0e557095da07
Neil Robert McGuiness <neilmcguiness91@gmail.com>
2024-04-20 15:43:47 +0200

    Audio Manager added

--- Assets/Shot.cs
+++ Assets/Shot.cs
@@ -8,6 +8,7 @@ public class Shot : MonoBehaviour
     public bool isDamaging;
     private SpriteRenderer sp;
     [SerializeField] private float speed = 4;
+    [SerializeField] FMODUnity.EventReference reflectSound;
 
     private void Start()
     {
@@ -45,6 +46,7 @@ public class Shot : MonoBehaviour
 
         isDamaging = !isDamaging;
         sp.color = isDamaging ? Color.red : Color.green;
+        GameManager.Instance.audioManager.playOneShot(reflectSound);
         //Vector2 inDirection = GetComponent<Rigidbody2D>().velocity;
         //Vector2 inNormal = col.contacts[0].normal;
         //Vector2 newVelocity = Vector2.Reflect(inDirection, inNormal) * speed;
